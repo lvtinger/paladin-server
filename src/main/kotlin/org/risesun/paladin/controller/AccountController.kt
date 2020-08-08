a4eb5acf3ab086e29694f48ff7fb26cd9e139f05
qiu.xu <xu.qiu@17zuoye.com>
2020-08-09 03:18:02 +0800

    fix

--- src/main/kotlin/org/risesun/paladin/controller/AccountController.kt
+++ src/main/kotlin/org/risesun/paladin/controller/AccountController.kt
@@ -1,5 +1,7 @@
 package org.risesun.paladin.controller
 
+import org.risesun.paladin.constant.MessageConstantValue
+import org.risesun.paladin.utils.RandomUtils
 import org.risesun.paladin.value.LoginRequestValue
 import org.risesun.paladin.value.Message
 import org.springframework.web.bind.annotation.RequestBody
@@ -12,6 +14,14 @@ import org.springframework.web.bind.annotation.RestController
 open class AccountController {
     @RequestMapping(value = ["/login.page"], method = [RequestMethod.POST])
     fun login(@RequestBody parameter: LoginRequestValue): Message<Any?> {
-        return Message.success(parameter)
+        return when {
+            parameter.account == "risesun"
+                    && parameter.password == "123456" -> {
+                Message.success(RandomUtils.uuid())
+            }
+            else -> {
+                Message.error("账号或者密码错误", MessageConstantValue.CODE_COMMON_ERROR)
+            }
+        }
     }
 }
