76cd181ec312c4cd32fc6e2677a971ac03e29ee0
qiu.xu <xu.qiu@17zuoye.com>
2020-08-19 03:51:46 +0800

    fix

--- src/main/kotlin/org/risesun/paladin/provider/api/service/AccountService.kt
+++ src/main/kotlin/org/risesun/paladin/provider/api/service/AccountService.kt
@@ -1,4 +1,8 @@
 package org.risesun.paladin.provider.api.service
 
+import org.risesun.paladin.provider.api.entity.Account
+
 interface AccountService {
+    fun login(account: String, password: String): Account
+    fun register(account: String, password: String): Account
 }
--- src/main/kotlin/org/risesun/paladin/provider/impl/service/AccountServiceImpl.kt
+++ src/main/kotlin/org/risesun/paladin/provider/impl/service/AccountServiceImpl.kt
@@ -1,6 +1,12 @@
 package org.risesun.paladin.provider.impl.service
 
 import org.risesun.paladin.provider.api.service.AccountService
+import org.risesun.paladin.provider.impl.repository.AccountRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
 
+@Service
 class AccountServiceImpl : AccountService {
+    @Autowired
+    private lateinit var accountRepository: AccountRepository
 }
