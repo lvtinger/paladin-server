f59aee0e181e0625176534d89c8d4a99dab45651
邱旭 <qiuxu@qiuxudeMacBook-Pro.local>
2021-02-11 14:31:58 +0800

    fix

--- src/main/java/org/lvtinger/paladin/relation/impl/RelationServiceImpl.java
+++ src/main/java/org/lvtinger/paladin/relation/impl/RelationServiceImpl.java
@@ -3,7 +3,6 @@ package org.lvtinger.paladin.relation.impl;
 import org.lvtinger.paladin.core.SpringBeanSupport;
 import org.lvtinger.paladin.relation.api.RelationService;
 import org.lvtinger.paladin.relation.impl.handler.RelationHandler;
-import org.lvtinger.paladin.relation.repo.RelationRepository;
 import org.lvtinger.paladin.value.Result;
 import org.springframework.stereotype.Service;
 
@@ -14,14 +13,7 @@ import java.util.Map;
 @Service
 public class RelationServiceImpl extends SpringBeanSupport implements RelationService {
 
-    private final RelationRepository relationRepository;
-
     private final Map<Integer, RelationHandler> relationHandlerHolder = new HashMap<>();
-
-
-    public RelationServiceImpl(RelationRepository relationRepository) {
-        this.relationRepository = relationRepository;
-    }
 
     @Override
     public Result<Boolean> make(Long ownerId, Long targetId, Integer type) {
@@ -35,6 +27,6 @@ public class RelationServiceImpl extends SpringBeanSupport implements RelationSe
     @Override
     public void afterPropertiesSet() {
         List<RelationHandler> relationHandlers = this.getBeanByType(RelationHandler.class);
-        relationHandlers.forEach(handler -> relationHandlers.set(handler.getType(), handler));
+        relationHandlers.forEach(handler -> relationHandlerHolder.put(handler.getType(), handler));
     }
 }
